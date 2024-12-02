c5bbe0b0116f55bf4de9162839cfed30289483fe
Obua Stella <stellaobua123@gmail.com>
2024-12-02 22:01:47 +0100

    added admission screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import AboutUs from "./Pages/AboutUs/AboutUs";
+import Admission from "./Pages/Admission/Admission";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/admission" element={<Admission />} />
       </Routes>
     </Router>
   );
